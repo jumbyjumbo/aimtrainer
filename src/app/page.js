32386c45618ad91e440821de68ff2a2d05d10d97
zxrl <me@zaksorel.com>
2024-03-05 23:35:01 -0500

    Refactor storeItems and purchaseItem function

--- src/app/page.js
+++ src/app/page.js
@@ -25,10 +25,14 @@ export default function Home() {
 
   // Store items
   const [storeItems, setStoreItems] = useState([
-    { id: 0, description: '+1 target', cost: 0.3, owned: 1 },
-    { id: 1, description: '+10% multiplier time', cost: 1, owned: 0 },
-    { id: 2, description: '+1 max multiplier', cost: 10, owned: 0 },
-    { id: 3, description: '+1 coin on hit', cost: 100, owned: 0 },
+    { id: 0, buff: '+1 target', baseCost: 0.42, owned: 0, growthRate: Math.E },
+    { id: 1, buff: '-10% combo decrease', baseCost: 0.69, owned: 0, growthRate: Math.E },
+    { id: 2, buff: '+1 max combo', baseCost: 1, owned: 0, growthRate: 1.07 },
+    { id: 3, buff: '+10% target size', baseCost: 11, owned: 0, growthRate: Math.E },
+    { id: 4, buff: '+1 coin on hit', baseCost: 99, owned: 0, growthRate: Math.E },
+    { id: 5, buff: '+10% combo growth', baseCost: 1000, owned: 0, growthRate: Math.E },
+    { id: 6, buff: '+10% coins', baseCost: 9999, owned: 0, growthRate: 1.07 },
+    { id: 7, buff: '+100% speed reward', baseCost: 10000, owned: 0, growthRate: 1.07 },
     // Add more store items here...
   ]);
 
@@ -128,39 +132,66 @@ export default function Home() {
     setCoinComboMultiplier(prevCoinComboMultiplier => Math.max(0, prevCoinComboMultiplier - 2000)); //lower coin multiplier by 2s
   };
 
-  // Function to purchase an item from the store
-  const purchaseItem = (itemId) => {
-    // Find the index of the item in the storeItems array
-    const itemIndex = storeItems.findIndex(item => item.id === itemId);
+  // get current cost of an item depending on how many owned
+  const calculateCurrentItemCost = (baseCost, growthRate, owned) => {
+    return parseFloat((baseCost * Math.pow(growthRate, owned)).toFixed(2));
+  };
 
-    if (itemIndex !== -1 && Coin >= storeItems[itemIndex].cost) {
-      // Extract the item using the found index
-      const item = storeItems[itemIndex];
-
-      // Perform actions based on the item description or other attributes
-      switch (item.description) {
-        case '+1 target':
-          addTarget();
-          break;
-        // Add more cases as needed
-      }
-
-      // Deduct coin cost from player's Coins
-      setCoin(prevCoin => prevCoin - item.cost);
-      console.log('purchased', item.description, 'for', item.cost, 'Coin');
-
-      // cost increases each purchase
-      const updatedStoreItems = storeItems.map((currentItem, index) => {
-        if (index === itemIndex) {
-          return { ...currentItem, cost: ((currentItem.cost * 5) ** 1.3).toFixed(2) };
-        }
-        return currentItem;
-      });
-
-      setStoreItems(updatedStoreItems);
-    } else {
+  // Function to apply the effects of a purchased item
+  const applyPurchasedItem = (buff) => {
+    switch (buff) {
+      case '+1 target':
+        addTarget();
+        break;
+      case '-10% combo decrease':
+        // Logic to reduce the combo decrease rate by 10%.
+        break;
+      case '+1 max combo':
+        // Logic to increase the maximum combo duration by 1 second.
+        break;
+      case '+10% target size':
+        // Logic to increase the target size by 10%.
+        break;
+      case '+1 coin on hit':
+        // Logic to increase coin earned per hit by 1.
+        break;
+      case '+10% combo growth':
+        // Logic to increase the combo growth rate by 10%.
+        break;
+      case '+10% coins':
+        // Logic to increase the coin earnings by 10%.
+        break;
+      case '+100% speed reward':
+        // Logic to double the speed reward.
+        break;
     }
   };
+
+
+
+  // Function to purchase an item from the store
+  const purchaseItem = (itemId) => {
+    const itemIndex = storeItems.findIndex(item => item.id === itemId);
+    if (itemIndex !== -1) {
+      const item = storeItems[itemIndex];
+      const currentCost = calculateCurrentItemCost(item.baseCost, item.growthRate, item.owned);
+      // Check if the player has enough Coin to purchase the item
+      if (Coin >= currentCost) {
+        setCoin(prevCoin => prevCoin - currentCost);
+        const updatedStoreItems = storeItems.map((currentItem, index) => {
+          // Apply the purchased item
+          if (index === itemIndex) {
+            // Apply the effects of the purchased item based on its description
+            applyPurchasedItem(item.description);
+            return { ...currentItem, owned: currentItem.owned + 1 };
+          }
+          return currentItem;
+        });
+        setStoreItems(updatedStoreItems);
+      }
+    }
+  };
+
 
   // on load function
   useEffect(() => {
@@ -182,32 +213,8 @@ export default function Home() {
   // Main game screen
   return (
     <main className="select-none bg-gray-200 text-[5vh] font-bold h-screen w-screen overflow-hidden" >
-      {/* coin combo multiplier progress bar */}
-      <div className="border-b-[3px] border-black absolute top-0 left-0 w-full h-[5vh] bg-[#F89414] bg-opacity-60 flex items-center">
-        <div className="h-full border-x-[3px] border-black bg-[#F89414]" style={{
-          width: `${(coinComboMultiplier / 10000) * 100}%`,
-        }}></div>
-        {/* Display current coin combo multiplier */}
-        {coinComboMultiplier > 0 && (
-          <div className="absolute top-0 left-0 right-0 h-full flex items-center justify-center">
-            <span className="text-[3vh]">COMBO x{(coinComboMultiplier / 1000).toFixed(2)}</span>
-          </div>
-        )}
-      </div>
-      {/* Target hit counter */}
-      <div className="absolute top-[3vh] left-1/2 transform -translate-x-1/2 text-center text-[10vh]">
-        {targetHitsCount}
-      </div>
-      {/* Coin counter */}
-      <div className="absolute top-[5vh] left-[2vw] flex items-center">
-        <img src="/btclogo.png" alt="BTC Logo" style={{ width: '5vh', height: '5vh' }} className="border-[3px] border-black rounded-full" />
-        <div style={{ width: '0.5vw' }}></div>
-        <div>
-          {Coin.toFixed(5)}
-        </div>
-      </div>
       {/* target spawn canvas */}
-      <div className="h-screen w-screen absolute" onMouseDown={onTargetMiss} style={{ cursor: "url('/greendot.png') 32 32, auto" }}>
+      <div className=" h-screen w-screen absolute" onMouseDown={onTargetMiss} style={{ cursor: "url('/greendot.png') 32 32, auto" }}>
         {/* target instances */}
         {targetPositions.map((targetPosition, targetID) => (
           <div
@@ -224,25 +231,65 @@ export default function Home() {
           />
         ))}
       </div>
+
+      { /* UI  */}
+      <div className="pointer-events-none">
+        { /* press space bar to open shop indicator  */}
+        <div className="text-[4vh] absolute bottom-[5vh] left-1/2 transform -translate-x-1/2 flex items-center justify-center ">
+          <img src="/spacebaricon.png" alt="Open Shop" style={{ width: '8vh', height: '8vh' }} />
+          <div style={{ width: '0.5vw' }}></div>
+          <span>SHOP</span>
+        </div>
+
+        {/* coin combo multiplier progress bar */}
+        <div className="border-b-[3px] border-black absolute top-0 left-0 w-full h-[5vh] bg-[#F89414] bg-opacity-60 flex items-center">
+          <div className="h-full border-x-[3px] border-black bg-[#F89414]" style={{
+            width: `${(coinComboMultiplier / 10000) * 100}%`,
+          }}></div>
+          {/* Display current coin combo multiplier */}
+          {coinComboMultiplier > 0 && (
+            <div className="absolute top-0 left-0 right-0 h-full flex items-center justify-center">
+              <span className="text-[3vh]">COMBO x{(coinComboMultiplier / 1000).toFixed(2)}</span>
+            </div>
+          )}
+        </div>
+
+        {/* Target hit counter */}
+        <div className="absolute top-[3vh] left-1/2 transform -translate-x-1/2 text-center text-[10vh]">
+          {targetHitsCount}
+        </div>
+
+        {/* Coin counter */}
+        <div className="absolute top-[5vh] left-[2vw] flex items-center">
+          <img src="/btclogo.png" alt="BTC Logo" style={{ width: '5vh', height: '5vh' }} className="border-[3px] border-black rounded-full" />
+          <div style={{ width: '0.5vw' }}></div>
+          <div>
+            {Coin.toFixed(5)}
+          </div>
+        </div>
+
+      </div>
+
+
       {/* coin store */}
       {isCoinStoreOpen && (
         <div className="absolute overflow-hidden w-screen h-[83.5vh] top-[16.5vh] bg-blue-500 flex flex-col bg-opacity-80">
           {/* "UPGRADE" text section */}
           <div className="bg-opacity-85 bg-blue-500 text-center py-[0.25vh] text-[4vh] border-t-[3px] border-b-[3px] border-black">STORE</div>
           {/* Grid section */}
-          <div className="grid grid-cols-5 grid-rows-3 gap-[1vh] flex-grow p-[2vh]">
+          <div className="grid grid-cols-5 grid-rows-3 gap-[2vh] flex-grow p-[2vh]">
             {storeItems.map((item, index) => (
               <div
                 key={item.id}
                 className="flex flex-col bg-green-200 px-[1vw] pt-[5vh] bg-opacity-70 border-[3px] border-black"
                 onMouseDown={() => purchaseItem(item.id)}
               >
-                <div className="flex-1 text-[4vh] self-center">{item.description}</div>
+                <div className="flex-1 text-[4vh] self-center">{item.buff}</div>
                 {/* Item cost */}
                 <div className="flex-1 flex items-center">
                   <img src="/btclogo.png" alt="BTC Logo" style={{ width: '5vh', height: '5vh' }} className="border-[3px] border-black rounded-full" />
                   <div style={{ width: '0.5vw' }}></div>
-                  <span>{item.cost}</span>
+                  <span>{calculateCurrentItemCost(item.baseCost, item.growthRate, item.owned)}</span>
                 </div>
               </div>
             ))}
