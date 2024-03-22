2bc3d854b7dca6e8c2ba5e7614505498f38ac1dd
zaksorel <sorelzakary@gmail.com>
2024-03-22 17:00:40 -0400

    Add font-bold to game title

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{ cursor: "url('/reddot.png') 32 32, auto" }}>
-      <body className='select-none  text-[5vh] font-helvetica uppercase font-bold'>{children}</body>
+      <body className='select-none text-[5vh] font-helvetica uppercase font-bold'>{children}</body>
     </html>
   );
 }
