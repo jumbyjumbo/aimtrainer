41564c3f8c92fcf259dc42da7e46231aa8deb879
zaksorel <sorelzakary@gmail.com>
2024-03-23 13:42:37 -0400

    Update HUD styling in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -985,7 +985,7 @@ export default function Home() {
       {/* HUD */}
       <div className="pointer-events-none">
         {/* Coin combo multiplier progress bar */}
-        <div className="backdrop-blur-sm border-b-[3px] border-black absolute top-0 left-0 w-full md:h-[5vh] h-[8vh] flex items-center" style={{ backgroundColor: `${comboBarColor.replace('rgb', 'rgba').replace(')', ', 0.65)')}` }}>
+        <div className="backdrop-blur-sm border-b-[3px] border-black absolute top-0 left-0 w-full md:h-[5vh] h-[7vh] flex items-center" style={{ backgroundColor: `${comboBarColor.replace('rgb', 'rgba').replace(')', ', 0.65)')}` }}>
           {/* Combo bar filler */}
           <div className={`h-full border-black ${combo == 0 ? '' : 'border-r-[3px]'} `} style={{ width: `${(combo / maxComboLimit) * 100}%`, backgroundColor: `${comboBarColor.replace('rgb', 'rgba').replace(')', ', 0.65)')}` }}></div>
           {/* Display current Coin combo multiplier */}
@@ -997,7 +997,7 @@ export default function Home() {
         </div>
 
         {/* Target hit and coin counter */}
-        <div className="md:text-[7vh] px-[2vw] absolute top-[5vh] w-full flex flex-row justify-between items-center">
+        <div className="text-[4vh] md:text-[7vh] px-[2vw] absolute top-[5vh] w-full flex flex-row justify-between items-center">
           <div>{formatAmount(Coin)} ₿</div>
           <div>{Score}</div>
         </div>
@@ -1020,7 +1020,7 @@ export default function Home() {
         )}
 
         {/* XP Progress Bar */}
-        <div className="backdrop-blur-sm border-t-[3px] border-black absolute bottom-0 left-0 w-full h-[8vh] md:h-[5vh] bg-[#076beb] bg-opacity-65 flex items-center">
+        <div className="backdrop-blur-sm border-t-[3px] border-black absolute bottom-0 left-0 w-full h-[6vh] md:h-[5vh] bg-[#076beb] bg-opacity-65 flex items-center">
           <div className={`h-full border-black bg-[#076beb] bg-opacity-50 ${playerProgress.currentXP == 0 ? '' : 'border-r-[3px]'}`} style={{ width: `${playerProgress.currentXP / ((XPNeededToLevelUp(playerProgress.currentLevel))) * 100}%` }}></div>
           {/* Display Level on the far left */}
           <div className="absolute left-0 h-full flex items-center px-4">
