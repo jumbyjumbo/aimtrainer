b867c198b322243f9de3ceab4e136e4e83feb656
zxrl <me@zaksorel.com>
2024-03-31 15:16:22 -0400

    Update body text color in RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{ cursor: "url('/reddot.png') 32 32, auto" }}>
-      <body className='bg-black select-none text-[5vh] font-helvetica uppercase font-bold overflow-hidden'>{children}</body>
+      <body className='bg-black text-white select-none text-[5vh] font-helvetica uppercase font-bold overflow-hidden'>{children}</body>
     </html>
   );
 }
